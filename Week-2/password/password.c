f602c29c3931efd1c4bcab4bc9e89a266c312858
Ledio <ledodurmishaj@gmail.com>
2024-08-23 10:53:20 +0200

    done

--- Week-2/password/password.c
+++ Week-2/password/password.c
@@ -1,4 +1,6 @@
-
+// Check that a password has at least one lowercase letter, uppercase letter, number and symbol
+// Practice iterating through a string
+// Practice using the ctype library
 
 #include <cs50.h>
 #include <ctype.h>
